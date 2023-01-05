2e2e240de260da27c425c21cafbc12a072ecb480
Thad Myrick <35581405+thadmyrick@users.noreply.github.com>
2023-01-05 07:55:14 -0600

    Day 6 Project of 100 Days of Code

---
+++ reeborgsworld/reeborg.py
@@ -0,0 +1,13 @@
+#Reeborgs World Maze Code Day 6 of 100 Days of code
+
+# def Escape():
+#     while not at_goal():
+#         if front_is_clear():
+#             move()
+#         elif wall_in_front() and wall_on_right():
+#             turn_left()
+#         elif wall_in_front() and not wall_on_right():
+#             turn_left()
+#             turn_left()
+#             turn_left()
+# Escape()
